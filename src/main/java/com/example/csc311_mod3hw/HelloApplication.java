bf3593b225dccf01866a4ef3567e52e13e28c041
abdul <Ahmedabdul362@gmail.com>
2025-02-13 15:32:48 -0500

    change scence size

--- src/main/java/com/example/csc311_mod3hw/HelloApplication.java
+++ src/main/java/com/example/csc311_mod3hw/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 500);  // Adjusted scene size for new layout
+        Scene scene = new Scene(fxmlLoader.load(), 540, 520);  // Adjusted scene size for new layout
         stage.setTitle("Loan Payment Calculator");
         stage.setScene(scene);
         stage.show();
